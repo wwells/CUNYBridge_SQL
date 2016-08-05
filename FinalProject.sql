bfcf61fa7a26fd3e0ab3854534f3a992cf90912a
wwells <walt@occ-data.org>
2016-08-05 09:29:54 -0500

    feat(site): init pages

---
+++ FinalProject.sql
@@ -0,0 +1,125 @@
+/* Walt Wells, CUNY Bridge, SQL Final Project:  08.2016*/
+
+/* 1) Create DB */
+CREATE DATABASE IF NOT EXISTS BuildingEnergy;
+
+/* 2) Create & populate initial tables */ 
+DROP TABLE IF EXISTS BuildingEnergy.EnergyCategories;
+DROP TABLE IF EXISTS BuildingEnergy.EnergyTypes;
+
+CREATE TABLE BuildingEnergy.EnergyCategories
+(
+  cat_id int PRIMARY KEY,
+  cat_name varchar(200) NOT NULL UNIQUE
+);
+
+CREATE TABLE BuildingEnergy.EnergyTypes
+(
+  type_id int PRIMARY KEY,
+  type_name varchar(200) NOT NULL UNIQUE,
+  cat_id int NULL REFERENCES EnergyCategories
+);
+
+INSERT INTO BuildingEnergy.EnergyCategories (cat_id, cat_name) 
+	VALUES ( 1, 'Fossil');
+INSERT INTO BuildingEnergy.EnergyCategories (cat_id, cat_name) 
+	VALUES ( 2, 'Renewable');
+
+INSERT INTO BuildingEnergy.EnergyTypes (type_id, type_name, cat_id) 
+	VALUES ( 1, 'Electricity', 1);
+INSERT INTO BuildingEnergy.EnergyTypes (type_id, type_name, cat_id) 
+    VALUES ( 2, 'Gas', 1);
+INSERT INTO BuildingEnergy.EnergyTypes (type_id, type_name, cat_id) 
+	VALUES ( 3, 'Steam', 1);
+INSERT INTO BuildingEnergy.EnergyTypes (type_id, type_name, cat_id) 
+	VALUES ( 4, 'Fuel Oil', 1);
+INSERT INTO BuildingEnergy.EnergyTypes (type_id, type_name, cat_id) 
+	VALUES ( 5, 'Solar', 2);
+INSERT INTO BuildingEnergy.EnergyTypes (type_id, type_name, cat_id) 
+	VALUES ( 6, 'Wind', 2);
+    
+/* 3) Join categories and energy types - do as a view for greater reuseability */
+DROP VIEW IF EXISTS BuildingEnergy.energyjoin;
+
+CREATE VIEW BuildingEnergy.energyjoin AS
+SELECT c.cat_name, t.type_name FROM BuildingEnergy.EnergyCategories c
+	LEFT JOIN BuildingEnergy.EnergyTypes t
+    ON t.cat_id = c.cat_id
+    ORDER BY t.type_name;
+
+SELECT * FROM BuildingEnergy.energyjoin;
+    
+/* 4)  Add Building table and Building/Energy link table */
+DROP TABLE IF EXISTS BuildingEnergy.Buildings;
+DROP TABLE IF EXISTS BuildingEnergy.BuildingEType;
+
+CREATE TABLE BuildingEnergy.Buildings
+(
+  build_id int PRIMARY KEY,
+  build_name varchar(200) NOT NULL UNIQUE
+);
+
+CREATE TABLE BuildingEnergy.BuildingEType
+(
+  build_id int NOT NULL REFERENCES BuildingEnergy.Buildings(build_id),
+  type_id int NOT NULL REFERENCES BuildingEnergy.EnergyTypes(type_id),
+  CONSTRAINT pk_BuildingEType PRIMARY KEY(build_id, type_id)
+ );
+ 
+INSERT INTO BuildingEnergy.Buildings (build_id, build_name)
+	VALUES (1, 'Empire State Building');
+INSERT INTO BuildingEnergy.Buildings (build_id, build_name)
+	VALUES (2, 'Chrysler Building');
+INSERT INTO BuildingEnergy.Buildings (build_id, build_name)
+	VALUES (3, 'Borough of Manhattan Community College');
+
+INSERT INTO BuildingEnergy.BuildingEType (build_id, type_id)
+	VALUES (1, 1);
+INSERT INTO BuildingEnergy.BuildingEType (build_id, type_id)
+	VALUES (1, 2);
+INSERT INTO BuildingEnergy.BuildingEType (build_id, type_id)
+	VALUES (1, 3);
+INSERT INTO BuildingEnergy.BuildingEType (build_id, type_id)
+	VALUES (2, 1);
+INSERT INTO BuildingEnergy.BuildingEType (build_id, type_id)
+	VALUES (2, 3);
+INSERT INTO BuildingEnergy.BuildingEType (build_id, type_id)
+	VALUES (3, 1);
+INSERT INTO BuildingEnergy.BuildingEType (build_id, type_id)
+	VALUES (3, 3);
+INSERT INTO BuildingEnergy.BuildingEType (build_id, type_id)
+	VALUES (3, 5);
+
+/* 5) show building and associated energy types */
+DROP VIEW IF EXISTS BuildingEnergy.buildEjoin; 
+
+CREATE VIEW BuildingEnergy.buildEjoin AS
+SELECT b.build_name, e.type_name FROM BuildingEnergy.Buildings b
+	LEFT JOIN BuildingEnergy.BuildingEType be
+		ON b.build_id = be.build_id
+	LEFT JOIN BuildingEnergy.EnergyTypes e
+		ON be.type_id = e.type_id;
+        
+SELECT * FROM BuildingEnergy.buildEjoin;
+        
+/* 6) add new information */
+INSERT INTO BuildingEnergy.EnergyTypes (type_id, type_name, cat_id) 
+	VALUES ( 7, 'Geothermal', 2);
+    
+SELECT * FROM BuildingEnergy.energyjoin;
+
+INSERT INTO BuildingEnergy.Buildings (build_id, build_name)
+	VALUES (4, 'Bronx Lion House');
+INSERT INTO BuildingEnergy.Buildings (build_id, build_name)
+	VALUES (5, 'Brooklyn Childrens Museum');
+    
+INSERT INTO BuildingEnergy.BuildingEType (build_id, type_id)
+	VALUES (4, 7);
+INSERT INTO BuildingEnergy.BuildingEType (build_id, type_id)
+	VALUES (5, 7);
+INSERT INTO BuildingEnergy.BuildingEType (build_id, type_id)
+	VALUES (5, 1);
+    
+SELECT * FROM BuildingEnergy.buildEjoin;
+
+/* 7) Create a query that shows only buildings that use renewable energy */
---
+++ README.md
@@ -0,0 +1,2 @@
+# CUNYBridge_SQL
+This Repo contains HW, Final Project, and gh-pages for CUNY MS in Data Analytics Bridge Program, SQL Module
